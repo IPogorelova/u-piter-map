b52c2c4ad0770fbcfd65c1f9f1fdc82c416a0076
inna.abdrakhmanova <inna.abdrakhmanova@jetbrains.com>
2021-08-05 20:10:23 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "u-piter-map",
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-gatsby-cloud"],
+};
